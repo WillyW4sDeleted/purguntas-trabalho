b8fc30ac599bae9010b47e86f1736e05b9b9dfb2
WillyW4sDeleted <145406289+WillyW4sDeleted@users.noreply.github.com>
2025-06-25 12:07:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,112 @@
 const perguntas = [
   {
-   enunciado: " ",
-    alternativas: 
-
-    
-  }
-]
+   enunciado: "1.  ",
+    alternativas: [
+      {
+        texto: " ",
+        afirmaçao: " "
+      },
+        {
+          texto: " "
+          afirmaçao: " "
+        }
+      ]
+  },
+  {
+     enunciado: "2. ",
+    alternativas: [
+      {
+        texto: " ",
+        afirmaçao: " "
+      },
+        {
+          texto: " "
+          afirmaçao: " "
+        }
+      ]
+  },
+  {
+     enunciado: "3.  ",
+    alternativas: [
+      {
+        texto: " ",
+        afirmaçao: " "
+      },
+        {
+          texto: " "
+          afirmaçao: " "
+        }
+       {
+        texto: " ",
+        afirmaçao: " "
+      },
+       {
+        texto: " ",
+        afirmaçao: " "
+      },
+      ]
+  },
+  {
+     enunciado: "4.  ",
+    alternativas: [
+      {
+        texto: " ",
+        afirmaçao: " "
+      },
+        {
+          texto: " "
+          afirmaçao: " "
+        }
+       {
+        texto: " ",
+        afirmaçao: " "
+      },
+       {
+        texto: " ",
+        afirmaçao: " "
+      },
+       {
+        texto: " ",
+        afirmaçao: " "
+      },
+      ]
+  },
+  {
+     enunciado: "5.  ",
+    alternativas: [
+      {
+        texto: " ",
+        afirmaçao: " "
+      },
+        {
+          texto: " "
+          afirmaçao: " "
+        }
+       {
+        texto: " ",
+        afirmaçao: " "
+      },
+      ]
+  },
+  {
+     enunciado: "6. Em qual parte você se arrepende? ",
+    alternativas: [
+      {
+        texto: " em tudo ",
+        afirmaçao: " "
+      },
+        {
+          texto: " em nada "
+          afirmaçao: " "
+        }
+       {
+        texto: " algumas partes ",
+        afirmaçao: " "
+      },
+       {
+        texto: "  ",
+        afirmaçao: " "
+      },
+      ]
+  },
+  ]
